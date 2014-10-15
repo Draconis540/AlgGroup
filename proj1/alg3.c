92fb666ae437ff39c63bb19dd1f9d4e90cfdc300
Nick Voigt <starboardtitan@gmail.com>
2014-10-15 16:08:04 -0700

    Adding start of alg3

---
+++ proj1/alg3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <utime.h>
+#include <string.h>
+#include <dirent.h>
+#include <math.h>
+#include <sys/un.h>
+
+
+int main(){
+
+    int n = 5;
+ int i, j, k;
+ int m[n] = {-2, -1, 0, 1, 2};
+ int b[n] = {9, 27, 54, 95, 96};
+ int v[n] = {1, 1, 1, 1, 1};
+
+
+
+ for(i=2; i<n; i++){
+
+
+ }
+
+
+return(0);
+}
+
+m[j]*(b[j]-b[k])+b[j]*(m[k]-m[j])>m[i]*(b[j]-b[k])+b[i]*(m[k]-m[j])
